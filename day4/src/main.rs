232219ced9c37aee9414fd81ee22d95d18c1429c
Lucas Alvarez <lucasalvarez@pm.me>
2023-07-24 18:56:01 -0400

    Day 4 Complete
    Add part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,22 +1,28 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
     let _filename = "C:\\Users\\lucas\\repos\\advent-of-code-2022\\day4\\src\\input.txt";
     let _sample_filename =
         "C:\\Users\\lucas\\repos\\advent-of-code-2022\\day4\\src\\sample_input.txt";
 
-    let mut number_of_overlapping_assignment_pairs = 0;
+    let mut part_1_answer = 0;
+    let mut part_2_answer = 0;
 
     for line in fs::read_to_string(_filename).unwrap().lines() {
         let (elf1, elf2) = parse_line_into_elves(line);
         if elf1.contains(&elf2) || elf2.contains(&elf1) {
-            number_of_overlapping_assignment_pairs += 1;
+            part_1_answer += 1;
         };
+        if elf1.overlaps(&elf2) {
+            part_2_answer += 1;
+        }
     }
 
-    println!("The number of overlapping assignments is {number_of_overlapping_assignment_pairs}");
+    println!("Part one: {part_1_answer}");
+    println!("Part two: {part_2_answer}");
 }
 
+#[derive(Debug)]
 struct Elf {
     start_range: i32,
     end_range: i32,
@@ -29,8 +35,19 @@ impl Elf {
             end_range,
         }
     }
+
     fn contains(&self, other: &Elf) -> bool {
         self.start_range >= other.start_range && self.end_range <= other.end_range
+    }
+
+    fn overlaps(&self, other: &Elf) -> bool {
+        let self_range = self.start_range..=self.end_range;
+        let other_range = other.start_range..=other.end_range;
+
+        let self_set: HashSet<i32> = self_range.collect();
+        let other_set: HashSet<i32> = other_range.collect();
+
+        return self_set.intersection(&other_set).count() > 0;
     }
 }
 
